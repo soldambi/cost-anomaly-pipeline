b3f40db8657cf2d618ac29577aba82e9159ea1d5
soldambi <57607047+soldambi@users.noreply.github.com>
2022-08-18 17:14:36 +0900

    refactoring
    - 미사용 import 제거
    - awswrangler 제거, 기존 라이브러리(boto3)로 대체
    - 반복되는 코드 함수화(s3 direct save)
    - pep8 적용

--- py/preprocessing.py
+++ py/preprocessing.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-import json
-import os
-import pandas as pd
-from datetime import datetime, timedelta
-import awswrangler as wr
-import boto3
-import io
 from pyathena import connect
+from io import StringIO
 
-conn = connect(s3_staging_dir="s3://s3-data-dev-athena--cost-aws-opsnow/tbil_aws_d/",
-               region_name="ap-northeast-2")
+import pandas as pd
+import boto3
 
-data = pd.read_sql_query('''
+s3 = boto3.client('s3')
+
+
+def save_df_on_s3(df, bucket, filepath):
+    csv_buffer = StringIO()
+    df.to_csv(csv_buffer, header=True, index=False)
+    csv_buffer.seek(0)
+    s3.put_object(Bucket=bucket, Body=csv_buffer.getvalue(), Key=filepath)
+    print(f'Saved {len(df)} rows on s3://{bucket}/{filepath}, Done!')
+
+
+bucket = 's3-dev-cost-anomaly--aiml'
+prefix = 'input/'
+output_fcst = 'fcst_usage_data.csv'
+output_pca = 'pca_usage_data.csv'
+
+conn = connect(
+    s3_staging_dir="s3://s3-data-dev-athena--cost-aws-opsnow/tbil_aws_d/",
+    region_name="ap-northeast-2"
+)
+
+data = pd.read_sql_query(
+    '''
     select
         CompanyID          as cmpn_id,
         PayerAccountID     as payr_acc_id,
@@ -51,7 +66,8 @@ data = pd.read_sql_query('''
             de.APLY_YN = 'Y'
             and de.CHRG_YEAR >= cast(YEAR(date_add('week', -3, now())) as varchar)
             and de.CHRG_MNTH >= lpad(cast(MONTH(date_add('week', -3, now())) as varchar), 2, '0')
-            and de.USE_ST_DT >= date_add('week', -3, now()) )
+            and de.USE_ST_DT >= date_add('week', -3, now()) 
+    )
     group by
         CompanyID,
         PayerAccountID,
@@ -62,83 +78,90 @@ data = pd.read_sql_query('''
         Operation,
         UsageDate,
         LineItemType
-        ''',
-    conn)
+    ''',
+    conn
+)
 
 data = data.fillna(
     value={
-        'cost'        : 0,
-        'charge_type' : 'N/A',
-        'usage_type'  : 'N/A',
-        'service_nm'  : 'N/A',
-        'region'      : 'N/A',
-        'operation'   : 'N/A'
+        'cost': 0,
+        'charge_type': 'N/A',
+        'usage_type': 'N/A',
+        'service_nm': 'N/A',
+        'region': 'N/A',
+        'operation': 'N/A'
     }
 )
 
 usage_data = data[data['charge_type'].isin(['Usage', 'DiscountedUsage'])]
 
-usage_df = usage_data[['cmpn_id','use_st_dt']].drop_duplicates()
+usage_df = usage_data[['cmpn_id', 'use_st_dt']].drop_duplicates()
 
 input_data = data[~data['charge_type'].isin(['Credit', 'Refund'])]
 
-usage_data_max_date = usage_data.groupby('cmpn_id', as_index=False).agg({'use_st_dt': 'max'})
+usage_data_max_date = usage_data.groupby(
+    'cmpn_id', as_index=False).agg({'use_st_dt': 'max'})
 
 usage_merged = input_data.merge(
     usage_df,
     on=['cmpn_id', 'use_st_dt'],
-    how='outer', 
+    how='outer',
     indicator=True
 )
 
-usage_include_sp_merged = usage_merged[usage_merged['_merge'] == 'both'].drop(columns=['_merge'])
+usage_include_sp_merged = usage_merged[usage_merged['_merge'] == 'both'].drop(columns=[
+                                                                              '_merge'])
 
 max_date_merged = usage_include_sp_merged.merge(
     usage_data_max_date,
     on=['cmpn_id', 'use_st_dt'],
-    how='outer', 
+    how='outer',
     indicator=True
 )
 
-usage_data_without_max_date = max_date_merged[max_date_merged['_merge'] == 'left_only'].drop(columns=['_merge'])
+usage_data_without_max_date = max_date_merged[max_date_merged['_merge'] == 'left_only'].drop(columns=[
+                                                                                             '_merge'])
 
-usage_data_without_max_date = usage_data_without_max_date[(usage_data_without_max_date.charge_type !='Credit') & 
-        (usage_data_without_max_date.charge_type != 'Refund') & 
-        (usage_data_without_max_date.charge_type != 'SavingsPlanCoveredUsage') & 
-        (usage_data_without_max_date.charge_type != 'SavingsPlanNegation')]
+usage_data_without_max_date = usage_data_without_max_date[
+    (usage_data_without_max_date.charge_type != 'Credit') &
+    (usage_data_without_max_date.charge_type != 'Refund') &
+    (usage_data_without_max_date.charge_type != 'SavingsPlanCoveredUsage') &
+    (usage_data_without_max_date.charge_type != 'SavingsPlanNegation')
+]
 
-usage_data_without_max_date['use_st_dt'] = pd.to_datetime(usage_data_without_max_date['use_st_dt'])
+usage_data_without_max_date['use_st_dt'] = pd.to_datetime(
+    usage_data_without_max_date['use_st_dt'])
 
-batch_date = datetime.today().strftime('%Y-%m-%d')
-length = 7*2
-horizon = 7
-levels = [0.01,0.1,0.2]
 
-cost_var = usage_data_without_max_date.groupby('cmpn_id', as_index=False).agg({'cost': 'var'}).rename(columns={"cost":"cost_var"}) 
+cost_var = usage_data_without_max_date.groupby('cmpn_id', as_index=False).agg(
+    {'cost': 'var'}).rename(columns={"cost": "cost_var"})
 
 zero_var = cost_var[cost_var['cost_var'] == 0]
 
 zero_var_merged = usage_data_without_max_date.merge(
     zero_var['cmpn_id'],
     on=['cmpn_id'],
-    how='outer', 
+    how='outer',
     indicator=True
 )
 
-pca_usage_data = zero_var_merged[zero_var_merged['_merge'] == 'left_only'].drop(columns=['_merge'])
+pca_usage_data = zero_var_merged[zero_var_merged['_merge'] == 'left_only'].drop(
+    columns=['_merge'])
 
-zero_var_data = zero_var_merged[zero_var_merged['_merge'] == 'both'].drop(columns=['_merge'])
+zero_var_data = zero_var_merged[zero_var_merged['_merge'] == 'both'].drop(columns=[
+                                                                          '_merge'])
 
 fcst_usage_data = usage_data_without_max_date
 
-pca_usage_data['ProductNameCode'] = pca_usage_data['cmpn_id']+'%%%'+pca_usage_data['payr_acc_id']+'%%%'+pca_usage_data['lnkd_acc_id']+'%%%'+ pca_usage_data['service_nm']+'%%%'+pca_usage_data['region']+'%%%'+pca_usage_data['charge_type']+'%%%'+pca_usage_data['usage_type']+'%%%'+pca_usage_data['operation']
+pca_usage_data['ProductNameCode'] = \
+    pca_usage_data['cmpn_id'] + '%%%' + \
+    pca_usage_data['payr_acc_id'] + '%%%' + \
+    pca_usage_data['lnkd_acc_id'] + '%%%' + \
+    pca_usage_data['service_nm'] + '%%%' + \
+    pca_usage_data['region'] + '%%%' + \
+    pca_usage_data['charge_type'] + '%%%' + \
+    pca_usage_data['usage_type'] + '%%%' + \
+    pca_usage_data['operation']
 
-S3_BUCKET = 's3-dev-cost-anomaly--aiml'
-PREFIX = 'input'
-FCST_INPUT_FILENAME = 'fcst_usage_data.csv'
-PCA_INPUT_FILENAME = 'pca_usage_data.csv'
-
-url = 's3://{}/{}/{}'.format(S3_BUCKET, PREFIX, FCST_INPUT_FILENAME)
-wr.s3.to_csv(fcst_usage_data, path=url, index=False)
-url = 's3://{}/{}/{}'.format(S3_BUCKET, PREFIX, PCA_INPUT_FILENAME)
-wr.s3.to_csv(pca_usage_data, path=url, index=False)
+save_df_on_s3(df=fcst_usage_data, bucket=bucket, filepath=prefix+output_fcst)
+save_df_on_s3(df=pca_usage_data, bucket=bucket, filepath=prefix+output_pca)
